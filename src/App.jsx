786406fa488bae7ca7f1f8a86da87ed52395399f
AwabSaif <awabtaz9090@gmail.com>
2023-07-19 14:00:25 +0300

    more add in site

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import React from 'react';
-import { BrowserRouter , Routes , Route ,Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link ,Outlet } from 'react-router-dom';
 import Home from './Pages/Home/Home';
 import About from './Pages/About/About';
 import "./Server/server"
@@ -10,9 +10,12 @@ import Dashboard from './Pages/Host/Dashboard/Dashboard';
 import Income from './Pages/Host/Income/Income';
 import HostVans from './Pages/Host/HostVans/HostVans';
 import HostVanDetail from './Pages/Host/HostVans/HostVansDetail/HostVanDetail';
+import HostVanInfo from './Pages/Host/HostVanInfo/HostVanInfo';
+import HostVanPhotos from './Pages/Host/HostVanInfo/HostVanPhotos';
+import HostVanPricing from './Pages/Host/HostVanInfo/HostVanPricing';
 import Reviews from './Pages/Host/Reviews/Reviews';
-import Container from"./components/Container/Container"
-import HostLayout from"./components/HostLayout/HostLayout"
+import Container from "./components/Container/Container"
+import HostLayout from "./components/HostLayout/HostLayout"
 
 
 
@@ -22,25 +25,33 @@ import HostLayout from"./components/HostLayout/HostLayout"
 
 export default function App() {
 
-  
+
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Container />}>
-        <Route index element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="vans" element={<Vans />} />
-        <Route path="vans/:id" element={<DetailVan />} />
-        
-        <Route path="host" element={<HostLayout />}>
-          <Route index element={<Dashboard />} />
-          <Route path="income" element={<Income />} />
-          <Route path="reviews" element={<Reviews />} />
-          <Route path="vans" element={<HostVans />} />
-          <Route path="vans/:id" element={<HostVanDetail />} />
+      <Routes>
+        <Route path="/" element={<Container />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="vans" element={<Vans />} />
+          <Route path="vans/:id" element={<DetailVan />} />
+
+          <Route path="host" element={<HostLayout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="income" element={<Income />} />
+            <Route path="reviews" element={<Reviews />} />
+            <Route path="vans" element={<Outlet />}>
+            <Route index element={<HostVans />} />
+            <Route path=":id" element={<HostVanDetail />} >
+                <Route index element={<HostVanInfo/>} />
+              <Route path="pricing" element={<HostVanPhotos/>} />
+              <Route path="photos" element={<HostVanPricing/>} />
+                  
+            </Route>
+
+            </Route>
+          </Route>
         </Route>
-      </Route>
-    </Routes>
-  </BrowserRouter>
+      </Routes>
+    </BrowserRouter>
   )
 }
--- src/Pages/Host/HostVans/HostVansDetail/HostVanDetail.jsx
+++ src/Pages/Host/HostVans/HostVansDetail/HostVanDetail.jsx
@@ -1,16 +1,22 @@
 import {React ,useEffect ,useState} from "react"
-import { useParams, Link } from "react-router-dom"
+import { useParams, Link, NavLink ,Outlet } from "react-router-dom"
 import './HostVansDetail.css'
 
 export default function HostVanDetail() {
     const { id } = useParams()
-    const [currentVan, setCurrentVan] =useState(null)
+    const [currentVan, setCurrentVan] =useState()
+    const ActiveStyle ={
+        fontWeight: "bold",
+        textDecoration: "underline",
+        color: "#161616",
+    
+    } 
 
    useEffect(() => {
         fetch(`/api/host/vans/${id}`)
             .then(res => res.json())
             .then(data => setCurrentVan(data.vans))
-    }, [])
+        }, [])
 
     if (!currentVan) {
         return <h1>Loading...</h1>
@@ -18,20 +24,32 @@ export default function HostVanDetail() {
 
     return (
         <section>
-            <div className="host-van-detail-layout-container">
-                <div className="host-van-detail">
-                    <img src={currentVan.imageUrl} />
-                    <div className="host-van-detail-info-text">
-                        <i
-                            className={`van-type van-type-${currentVan.type}`}
-                        >
-                            {currentVan.type}
-                        </i>
-                        <h3>{currentVan.name}</h3>
-                        <h4>${currentVan.price}/day</h4>
-                    </div>
+        <Link
+            to=".."
+            relative="path"
+            className="back-button"
+        >&larr; <span>Back to all vans</span></Link>
+
+        <div className="host-van-detail-layout-container">
+            <div className="host-van-detail">
+                <img src={currentVan.imageUrl} />
+                <div className="host-van-detail-info-text">
+                    <i
+                        className={`van-type van-type-${currentVan.type}`}
+                    >
+                        {currentVan.type}
+                    </i>
+                    <h3>{currentVan.name}</h3>
+                    <h4>${currentVan.price}/day</h4>
                 </div>
             </div>
+            <nav className="host-van-detail-nav">
+               <NavLink to='.' style={({isActive}) => isActive ? ActiveStyle :null} end >Details</NavLink>
+               <NavLink to='pricing' style={({isActive}) => isActive ? ActiveStyle :null} >Pricing</NavLink>
+               <NavLink to='photos' style={({isActive}) => isActive ? ActiveStyle :null} >Photos</NavLink>
+                </nav>
+            <Outlet />
+        </div>
         </section>
     )
 }
