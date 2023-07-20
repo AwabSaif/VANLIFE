00f38b61a71be0f55b5e0226639a753cb56d05ae
AwabSaif <awabtaz9090@gmail.com>
2023-07-20 14:48:49 +0300

    add Host

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -4,12 +4,14 @@ import { useNavigate , useLocation } from "react-router-dom"
 import { loginUser } from "../../API/api"
 
 export default function Login() {
-    const [loginFormData, setLoginFormData] =useState({ email: "", password: "" })
-    const [status, setStatus] =useState("idle")
-    const [error, setError] =useState(null)
+    const [loginFormData, setLoginFormData] = useState({ email: "", password: "" })
+    const [status, setStatus] = useState("idle")
+    const [error, setError] = useState(null)
 
     const location = useLocation()
     const navigate = useNavigate()
+
+    const from = location.state?.from || "/host";
 
     function handleSubmit(e) {
         e.preventDefault()
@@ -18,7 +20,7 @@ export default function Login() {
             .then(data => {
                 setError(null)
                 localStorage.setItem("loggedin", true)
-                navigate("/host",{ replace : true})
+                navigate(from, { replace: true })
             })
             .catch(err => {
                 setError(err)
@@ -39,12 +41,12 @@ export default function Login() {
     return (
         <div className="login-container">
             {
-                location.state ?.message &&
+                location.state?.message &&
                     <h3 className="login-error">{location.state.message}</h3>
             }
             <h1>Sign in to your account</h1>
             {
-                error ?.message &&
+                error?.message &&
                     <h3 className="login-error">{error.message}</h3>
             }
 
--- src/components/AuthRequired/AuthRequired.jsx
+++ src/components/AuthRequired/AuthRequired.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
-import { Outlet , Navigate} from 'react-router-dom'
+import { Outlet , Navigate , useLocation}  from 'react-router-dom'
 import HostLayout from '../HostLayout/HostLayout'
 
 export default function AuthRequired() {
-
     const authenticated = localStorage.getItem("loggedin") 
+    const loaction = useLocation()
+    console.log(loaction)
 
     if(!authenticated){
 
-        return <Navigate to='/login'  replace = {true} state={{message: "You must log in first"}}/>
+        return (<Navigate 
+            to='/login'  
+            state={{
+                message: "You must log in first",
+                from: loaction.pathname
+            }}
+            replace 
+            />)
     }
     return <Outlet/>
 }
